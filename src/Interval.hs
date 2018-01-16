3b1f5a86884ec7937809d244e4f9fb2b1a597f3b
nachivpn <nachivpn@gmail.com>
2018-01-16 23:18:54 +0100

    adds interval partitioning

--- src/Interval.hs
+++ src/Interval.hs
@@ -2,27 +2,43 @@ module Interval where
 
 type Interval a = (a,a)
 
-isLt :: Ord a => Interval a ->  Interval a -> Bool
-isLt (_,b) (_,d) = b < d
+isLt :: Ord a => (Interval a -> a) -> Interval a ->  Interval a -> Bool
+isLt f x y = f x < f y
 
 isOverlap :: Ord a => Interval a -> Interval a -> Bool
 isOverlap (_,b) (c,_) = b >= c
 
-isort :: Ord a => [Interval a] -> [Interval a] 
-isort [] = []
-isort (i@(a,b):xs) = isort left ++ i : isort right
+isOverlapL :: Ord a => Interval a -> [Interval a] -> Bool
+isOverlapL x = or . map (isOverlap x)
+
+isort :: Ord a => (Interval a -> a) -> [Interval a] -> [Interval a] 
+isort f []              = []
+isort f (i@(a,b):xs)    = isort f left ++ i : isort f right
     where
-        left = [ x | x <- xs,  x `isLt` i]
-        right = [ x | x <- xs, not (x `isLt` i)]
+        left = [ x | x <- xs,  x `isLtf` i]
+        right = [ x | x <- xs, not $ x `isLtf` i]
+        isLtf = isLt f
 
 interval_scheduling :: Ord a => 
     [Interval a]        -- | list of itervals
     -> [Interval a]     -- | list of intervals that are pairwise disjoint in sorted order
-interval_scheduling = reverse . is [] . isort
+interval_scheduling = reverse . is [] . isort snd
     where
         is ss []     = ss
-        is ss (x:xs) = is (x : ss) (removeOverlapping x xs)
-        removeOverlapping x = dropWhile (isOverlap x)
+        is ss (x:xs) = is (x : ss) (removeOverlap x xs)
+        removeOverlap x = dropWhile (isOverlap x)
+
+interval_partitioning :: (Eq a, Ord a) => 
+    [Interval a]        -- | list of intervals
+    -> [[Interval a]]   -- | list of (list of intervals that are pairwise disjoint)
+interval_partitioning = ip [] . isort fst 
+        where
+            ip ss [] = ss
+            ip ss (x:xs) = ip (add x ss) xs
+            add x [] = [[x]]
+            add x (s : ss)
+                | x `isOverlapL` s  = s : add x ss
+                | otherwise         = (x : s) : ss
 
 -- | Tests
 disjointIntervals :: Ord a => [Interval a] -> Bool
@@ -33,3 +49,6 @@ disjointIntervals (x:xs) = (isDisjoint x xs) && (disjointIntervals xs)
 
 prop_is_safety :: [Interval Int] -> Bool
 prop_is_safety = disjointIntervals . interval_scheduling
+
+prop_ip_safety :: [Interval Int] -> Bool
+prop_ip_safety = and . map disjointIntervals . interval_partitioning
