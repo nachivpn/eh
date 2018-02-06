c3f70f28db4bef9d2010973396c060ad7ce8baa4
nachivpn <nachivpn@gmail.com>
2018-02-06 21:15:32 +0100

    removes bug in substring match

--- src/Trie.hs
+++ src/Trie.hs
@@ -2,37 +2,35 @@ module Trie where
 
 import qualified Data.Map.Strict as Map
 
-data Trie a = Node (Map.Map a (Trie a)) | Leaf
+data Trie a = Node Bool (Map.Map a (Trie a)) | Leaf
     deriving Show
 
 type Dictionary = Trie Char
 
 mkTrie :: Ord a => [a] -> Trie a
-mkTrie [] = Leaf
-mkTrie (a:as) = Node (Map.insert a (mkTrie as) Map.empty)
+mkTrie [] = Node True Map.empty
+mkTrie (a:as) = Node False (Map.insert a (mkTrie as) Map.empty)
 
 insert :: Ord a => [a] -> Trie a -> Trie a
-insert []     tr            = tr
-insert as     Leaf          = mkTrie as
-insert (a:as) (Node trmap)  = 
+insert as     Leaf              = mkTrie as
+insert []     (Node b trmap)    = Node True trmap
+insert (a:as) (Node b trmap)    =
     case Map.lookup a trmap of
-        Nothing -> Node (Map.insert a (mkTrie as) trmap)
-        Just tr -> let tr' = insert as tr in
-            Node (Map.insert a tr' trmap)
+        Nothing -> Node b (Map.insert a (mkTrie as) trmap)
+        Just tr -> Node b (Map.insert a (insert as tr) trmap)
 
 exists :: Ord a => [a] -> Trie a -> Bool
-exists []     tr            = True
-exists as     Leaf          = False
-exists (a:as) (Node trmap)  = 
+exists _        Leaf            = False
+exists []       (Node b _)      = b
+exists (a:as)   (Node b trmap)  =
     case Map.lookup a trmap of
         Nothing -> False
         Just tr -> exists as tr
 
 patternMatch :: [Char] -> Dictionary -> Bool
-patternMatch []     Leaf          = True
-patternMatch []     _             = False
-patternMatch as     Leaf          = False
-patternMatch (a:as) (Node trmap)
+patternMatch _     Leaf          = False
+patternMatch []     (Node b _)   = b 
+patternMatch (a:as) (Node b trmap)
     | a == '*' = any (patternMatch as) (Map.elems trmap)
     | otherwise =
         case Map.lookup a trmap of
